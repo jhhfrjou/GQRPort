1154bf08b0d900b0f869c57796a0e18c3362b5f9
Unknown <jhhfrjou1@gmail.com>
2019-09-23 11:04:53 +0100

    This gqr now returns sql statements

--- src/tests/GQRTest.java
+++ src/tests/GQRTest.java
@@ -1,30 +1,36 @@
 import gqr.*;
 import org.junit.jupiter.api.*;
+import uk.ac.ox.cs.chaseBench.model.DatabaseSchema;
+import uk.ac.soton.ecs.RelationalModel.Exceptions.InconsistentAtomException;
 
 import java.io.File;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GQRTest {
-    GQR gqr;
+    GQR gqr = new GQR("resources/query_0.txt", "resources/views_for_q_0.txt", -1);
 
 
     @Test
     void reformulatePassingPaperQuery() {
         try {
-            gqr = new GQR("resources/paper_query.txt","resources/paper_views.txt", -1);
-            gqr.reformulate(gqr.getQuery());
-        } catch (NonAnswerableQueryException e) {
+            gqr = new GQR("resources/paper_query.txt", "resources/paper_views.txt", -1);
+            List<CompRewriting> rewrites = gqr.reformulate(gqr.getQueries().get(0));
+            rewrites.forEach(System.out::println);
+        } catch (NonAnswerableQueryException | InconsistentAtomException e) {
             fail();
         }
     }
 
     @Test
-    void reformulatePassingQ0() {
+    void reformulatePassingOBDAQ2Query() {
         try {
-            gqr = new GQR("resources/query_0.txt","resources/views_for_q_0.txt", -1);
-            gqr.reformulate(gqr.getQuery());
-        } catch (NonAnswerableQueryException e) {
+            gqr = new GQR("resources/Q2.txt", "resources/Q2-tgds.rule", -1);
+            List<CompRewriting> rewrites = gqr.reformulate(gqr.getQueries().get(0));
+            System.out.println(SQLCreator.manyQueriestoSQL(rewrites));
+
+        } catch (NonAnswerableQueryException | InconsistentAtomException e) {
             fail();
         }
     }
@@ -33,8 +39,8 @@ class GQRTest {
     void reformulatePassingQ55() {
         try {
             gqr = new GQR("resources/queryHD_55.txt", "resources/views_55.txt", -1);
-            gqr.reformulate(gqr.getQuery());
-        } catch (NonAnswerableQueryException e) {
+            gqr.reformulate(gqr.getQueries().get(0));
+        } catch (NonAnswerableQueryException | InconsistentAtomException e) {
             fail();
         }
     }
@@ -43,8 +49,8 @@ class GQRTest {
     void reformulatePassingQ59() {
         try {
             gqr = new GQR("resources/queryHD_59.txt", "resources/views_59.txt", -1);
-            gqr.reformulate(gqr.getQuery());
-        } catch (NonAnswerableQueryException e) {
+            gqr.reformulate(gqr.getQueries().get(0));
+        } catch (NonAnswerableQueryException | InconsistentAtomException e) {
             fail();
         }
     }
@@ -53,57 +59,52 @@ class GQRTest {
     @Test
     void reformulatePassingQ95() {
         try {
-            gqr = new GQR("resources/queryHD_95.txt","resources/views_95.txt", -1);
-            gqr.reformulate(gqr.getQuery());
-        } catch (NonAnswerableQueryException e) {
+            gqr = new GQR("resources/queryHD_95.txt", "resources/views_95.txt", -1);
+            gqr.reformulate(gqr.getQueries().get(0));
+        } catch (NonAnswerableQueryException | InconsistentAtomException e) {
             fail();
         }
     }
 
     @Test
     void reformulateFailPaperQuery() {
-        gqr = new GQR("resources/paper_query.txt","resources/paper_views.txt", 0);
+        gqr = new GQR("resources/paper_query.txt", "resources/paper_views.txt", 0);
         try {
-            gqr.reformulate(gqr.getQuery());
-            assertThrows(NonAnswerableQueryException.class,null);
-        } catch (NonAnswerableQueryException e) {
+            gqr.reformulate(gqr.getQueries().get(0));
+            assertThrows(NonAnswerableQueryException.class, null);
+        } catch (NonAnswerableQueryException | InconsistentAtomException e) {
 
         }
     }
 
     @Test
-    void reformulateFailQ0() {
-        try {
-            gqr = new GQR("resources/query_0.txt","resources/views_for_q_0.txt", 1);
-            gqr.reformulate(gqr.getQuery());
-            //assertThrows(NonAnswerableQueryException.class,null);
-        } catch (NonAnswerableQueryException e) {        }
-    }
-
-
-    @Test
     void reformulateFailQ55() {
         try {
-            gqr = new GQR("resources/queryHD_55.txt","resources/views_55.txt", 1);
-            gqr.reformulate(gqr.getQuery());
-            assertThrows(NonAnswerableQueryException.class,null);
-        } catch (NonAnswerableQueryException e) {        }
+            gqr = new GQR("resources/queryHD_55.txt", "resources/views_55.txt", 1);
+            gqr.reformulate(gqr.getQueries().get(0));
+            assertThrows(NonAnswerableQueryException.class, null);
+        } catch (NonAnswerableQueryException | InconsistentAtomException e) {
+        }
     }
+
     @Test
     void reformulateFailQ59() {
         try {
-            gqr = new GQR("resources/queryHD_59.txt","resources/views_59.txt", 1);
-            gqr.reformulate(gqr.getQuery());
-            assertThrows(NonAnswerableQueryException.class,null);
-        } catch (NonAnswerableQueryException e) {        }
+            gqr = new GQR("resources/queryHD_59.txt", "resources/views_59.txt", 1);
+            gqr.reformulate(gqr.getQueries().get(0));
+            assertThrows(NonAnswerableQueryException.class, null);
+        } catch (NonAnswerableQueryException | InconsistentAtomException e) {
+        }
     }
+
     @Test
     void reformulateFailQ95() {
         try {
-            gqr = new GQR("resources/queryHD_95.txt","resources/views_95.txt", 1);
-            gqr.reformulate(gqr.getQuery());
-            assertThrows(NonAnswerableQueryException.class,null);
-        } catch (NonAnswerableQueryException e) {        }
+            gqr = new GQR("resources/queryHD_95.txt", "resources/views_95.txt", 1);
+            gqr.reformulate(gqr.getQueries().get(0));
+            assertThrows(NonAnswerableQueryException.class, null);
+        } catch (NonAnswerableQueryException | InconsistentAtomException e) {
+        }
     }
 
     @Test
@@ -111,12 +112,14 @@ class GQRTest {
         File source = new File("tempSource.txt");
         File target = new File("tempTarget.txt");
         try {
-            uk.ac.ox.cs.chaseBench.model.DatabaseSchema dbSchema = GQR.generateSchema(GQR.readRulesfromFile("resources/paper_views.txt"));
-            dbSchema.save(target,true);
-            dbSchema.save(source,false);
+            uk.ac.ox.cs.chaseBench.model.DatabaseSchema dbSchema = new DatabaseSchema();
+            GQR.generateSchema(dbSchema, GQR.readRulesfromFile("resources/paper_views.txt"));
+            dbSchema.save(target, true);
+            dbSchema.save(source, false);
             uk.ac.ox.cs.chaseBench.model.DatabaseSchema dbSchema2 = new uk.ac.ox.cs.chaseBench.model.DatabaseSchema();
-            dbSchema2.load(target,true);
-            dbSchema2.load(source,false);
+
+            dbSchema2.load(target, true);
+            dbSchema2.load(source, false);
             source.delete();
             target.delete();
         } catch (Exception e) {
